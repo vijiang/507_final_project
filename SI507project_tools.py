739bed6b4edf239f409a364c95df9ce3ec3b1029
Vivian Jiang <vijiang@umich.edu>
2019-04-13 22:48:47 -0400

    working spotify code

---
+++ SI507project_tools.py
@@ -0,0 +1,24 @@
+import spotipy
+import json
+from spotipy.oauth2 import SpotifyClientCredentials
+from flask import Flask
+
+
+SPOTIFY_CLIENT_ID = "fc28786916ef479b977f7dabacfb68ab"
+SPOTIFY_SECRET = "764343631291468c9829c0274616b5f4"
+
+client_credentials_manager = SpotifyClientCredentials(
+    client_id=SPOTIFY_CLIENT_ID, client_secret=
+    SPOTIFY_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+username = "cariboutheband"
+playlist_id = "4Dg0J0ICj9kKTGDyFu0Cv4"
+
+results = sp.user_playlist(username, playlist_id)
+
+with open("caribou_tracks", 'w') as outfile:
+    json.dump(results, outfile, indent=4)
+
+
+
